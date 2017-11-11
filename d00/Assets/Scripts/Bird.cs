5d2a5c2bab2c3347528b2dcb81f12f92a94ff6f5
Leo SIMON <lsimon@e1r12p14.42.fr>
2017-11-11 15:50:49 +0100

    flappy bird on the way

---
+++ d00/Assets/Scripts/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	public float speedUp = 5f;
+	public float speedDown = 1f;
+	private bool move;
+	// Use this for initialization
+	void Start () {
+		move = true;
+	}
+	public void freeze() {
+		Debug.Log("hey");
+		move = false;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (move) {
+			if (Input.GetKey("space")) {
+				transform.Translate(Vector3.up * Time.deltaTime * speedUp);
+			} else {
+				transform.Translate(Vector3.down * Time.deltaTime * speedDown);
+			}
+		}
+	}
+}
---
+++ d00/Assets/Scripts/Pipe.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+	public Transform bird;
+	public Bird birdScript;
+	private float gapUp = 1.55f;
+	private float gapDown = 1.9f;
+	public float speed = 1f;
+	private float minY = 3;
+	private float maxY = 8;
+	private bool passed = false;
+	private bool move = true;
+	private int score = 0;
+
+	void respawn() {
+		Vector3 newPos = new Vector3(4, Random.Range(minY, maxY), 0);
+		transform.position = newPos;
+		passed = false;
+	}
+
+	void endGame() {
+		move = false;
+		Debug.Log("Score: " + score + "\nTime: 0s");
+		birdScript.freeze();
+	}
+	
+	void Update () {
+		if (move) {
+			transform.Translate(Vector3.left * Time.deltaTime * speed);
+			if (transform.position.x < -3 ) {
+				respawn();
+			}
+			if (transform.position.x < 0.8f && transform.position.x > -0.88f) {
+				if (bird.position.y < transform.position.y - gapUp || bird.position.y > transform.position.y + gapDown){
+					endGame();
+				}
+			}
+			if (transform.position.x < -1 && !passed){
+				score += 5;
+				passed = true;
+			}
+		}
+	}
+}
